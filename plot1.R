9274cdc7dd2218984bbb0a3e3fafd51ced08ea5e
Вадим <neformat@gmail.com>
2014-06-05 21:56:21 +0300

    Added first plot

---
+++ plot1.R
@@ -0,0 +1,10 @@
+url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+temp <- tempfile()
+download.file(url,temp)
+data <- read.table(unz(temp, "household_power_consumption.txt"), sep=";", header=TRUE, stringsAsFactors=FALSE, na.strings="?")
+unlink(temp)
+data$Date <- as.Date(data$Date,"%d/%m/%Y")
+plotdata <- subset(data, Date >= as.Date("2007-02-01") & Date < as.Date("2007-02-03"))
+png("plot1.png")
+hist(plotdata$Global_active_power, breaks = 12, col = "red", xlab = "Global Active Power (kilowatts)", ylab = "Frequency", main = "Global Active Power")
+dev.off()
