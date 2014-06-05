98e2edf865536118c7d0d7dbaed3a5cac71f2724
Вадим <neformat@gmail.com>
2014-06-05 22:08:39 +0300

    Added some annotations

--- plot1.R
+++ plot1.R
@@ -1,10 +1,19 @@
+# Load dataset from url to tempfile
 url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 temp <- tempfile()
 download.file(url,temp)
+#
+# Load date from unzipped file
 data <- read.table(unz(temp, "household_power_consumption.txt"), sep=";", header=TRUE, stringsAsFactors=FALSE, na.strings="?")
 unlink(temp)
+#
+# Format date
 data$Date <- as.Date(data$Date,"%d/%m/%Y")
+#
+# Subsetting 
 plotdata <- subset(data, Date >= as.Date("2007-02-01") & Date < as.Date("2007-02-03"))
+#
+# Make a plot
 png("plot1.png")
 hist(plotdata$Global_active_power, breaks = 12, col = "red", xlab = "Global Active Power (kilowatts)", ylab = "Frequency", main = "Global Active Power")
 dev.off()
